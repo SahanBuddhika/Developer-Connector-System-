dea333297559adc0cc802568140b59655a062195
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-02-16 01:50:34 +0530

    create routs

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI:
+        'mongodb://Sahan:Sa**1994@cluster0-shard-00-00-decgj.mongodb.net:27017,cluster0-shard-00-01-decgj.mongodb.net:27017,cluster0-shard-00-02-decgj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+        
+    secretOrKey:
+        'SECRET'
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routs/api/users');
+const posts = require('./routs/api/posts');
+const profile = require('./routs/api/profile');
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
+app.get('/',(req,res) => res.send('Hellow '));
+
+
+//use routes
+
+app.use('/api/users',users);
+app.use('/api/posts',posts);
+app.use('/api/profile',profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server Running on port ${port}`));
