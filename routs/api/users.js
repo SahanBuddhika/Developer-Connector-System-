fa741e0ca4082d9207717ca217fdc16e9dee0d56
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-02-16 08:53:50 +0530

    email password validation

--- routs/api/users.js
+++ routs/api/users.js
@@ -40,10 +40,38 @@ router.post('/register', (req,res) => {
                             .save()
                             .then(user => res.json(user))
                             .catch(err => console.log(err));
-                    })
-                })
+                    });
+                });
             }
-        })
+        });
 });
+
+// @route GET api/users/login
+// @desc Login User / Returning JWT Token
+// @access Public
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    User.findOne({email})
+        .then(user => {
+            //check for user
+            if(!user){
+                return res.status(404).json({email:'User not found'});
+            }
+
+            //check password
+            bcrypt.compare(password, user.password).then(isMatch => {
+                if(isMatch){
+                    res.json({msg: 'Success'});
+                }else{
+                    return res.status(400).json({password:'Password incorrect'});
+                }
+            });
+        });
+});
+
 
 module.exports = router;
