2cafee3a6dd3111825e3745199c9518b1fb70c95
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-02-19 18:45:12 +0530

    create a profile route

--- server.js
+++ server.js
@@ -3,43 +3,42 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-
 const users = require('./routs/api/users');
-const posts = require('./routs/api/posts');
 const profile = require('./routs/api/profile');
+const posts = require('./routs/api/posts');
 
 const app = express();
+
 
 mongoose.set('useNewUrlParser', true)
 //mongoose.set('useFindAndModify', true)
 //mongoose.set('useCreateIndex', true)
 
-
-
-//Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//DB Config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-//passport middleware
-app.use(passport.initialize());  
+// Passport middleware
+app.use(passport.initialize());
 
-//passport config
+// Passport Config
 require('./config/passport')(passport);
 
-//use routes
-app.use('/api/users',users);
-app.use('/api/posts',posts);
-app.use('/api/profile',profile);
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server Running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
