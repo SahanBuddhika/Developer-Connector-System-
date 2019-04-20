d2f3c657df4ee669e973b378861cc067c9afed08
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-04-20 07:44:15 +0530

    add Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+       <Navbar/>
+       <Footer/>
       </div>
     );
   }
